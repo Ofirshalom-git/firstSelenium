f6aed17d001425f6d96577cfb5b1c499b220ae05
ofish <ofirshalommota@walla.com>
2020-03-27 11:41:01 +0300

    before changin items infrastructure, with comments

--- Common/Basic comppenants/Items.cs
+++ Common/Basic comppenants/Items.cs
@@ -12,12 +12,12 @@ namespace Common
     {
 
         public List<Button> AddToCartButton => Driver.FindElements(By.CssSelector(".product-container .button-container .ajax_add_to_cart_button")).Select(element => new Button(Driver, element)).ToList();
-        public List<Button> ViewItemButton => Driver.FindElements(By.CssSelector(".lnk_view.btn.btn-default")).Select(element => new Button(Driver, element)).ToList();
+        public List<Button> ViewButtonOfItem => Driver.FindElements(By.CssSelector(".lnk_view.btn.btn-default")).Select(element => new Button(Driver, element)).ToList();
         public List<Button> QuickViewButton => Driver.FindElements(By.CssSelector(".quick-view")).Select(element => new Button(Driver, element)).ToList();
         public List<IWebElement> ItemsPrices => Driver.FindElements(By.CssSelector(".right-block .price.product-price")).ToList();
         //change it's Css Selector to the right one 
         public List<IWebElement> ItemsPickedColor => Driver.FindElements(By.CssSelector(".right-block .price.product-price")).ToList();
-        public List<ColorRow> ItemsOptionalColors => Driver.FindElements(By.CssSelector(".color_to_pick_list.clearfix")).Select(element => new ColorRow(Driver, element)).ToList();
+        public List<ColorRow> OptionalColorsOfItem => Driver.FindElements(By.CssSelector(".color_to_pick_list.clearfix")).Select(element => new ColorRow(Driver, element)).ToList();
 
         public Items(IWebDriver driver, IWebElement parentElement) : base(driver, parentElement)
         {
--- MyStoreAtomation/CatalogTests.cs
+++ MyStoreAtomation/CatalogTests.cs
@@ -19,6 +19,7 @@ namespace MyStoreAtomation
             var threwExeption = false;
             try
             {
+                //hover over item[0], try to access it's addToCart button, if it won't apper it will throw an exception.
                 var button = CatalogPage.RowContent.Items.HoverOnItemByIndex(0).RowContent.Items.AddToCartButton[0];
             }
 
@@ -36,6 +37,7 @@ namespace MyStoreAtomation
             var threwExeption = false;
             try
             {
+                //hover over item[0], try to access it's addToCart button, if it won't apper it will throw an exception.
                 var button = CatalogPage.RowContent.Items.HoverOnItemByIndex(0).RowContent.Items.AddToCartButton[0];
             }
 
@@ -50,27 +52,38 @@ namespace MyStoreAtomation
         [TestMethod]
         public void AddToCartButtonWorksTest()
         {
+            //click on cart button to count items
             var preAddcartPage = CatalogPage.Header.CartButton.Click<CartPage>();
             var preAddAmount = preAddcartPage.CartTable.GetQuantity();
+            //go to myStore
             var preCatalogPage = preAddcartPage.Header.MyStoreButton.Click<CatalogPage>();
+            //add the Item[0] to cart
             var postCatalogPage = preCatalogPage.RowContent.Items.HoverOnItemByIndex(0).RowContent.Items.AddToCartButton[0].Click<CatalogPage>();
+            //go to cart and count items
             var postAddAmount = postCatalogPage.Header.CartButton.Click<CartPage>().CartTable.GetQuantity();
+            //num of items should be 1 more
             postAddAmount.Should().Be(preAddAmount+1);
         }
 
         [TestMethod]
         public void PickedItemColorAppersTest()
         {
-            var pressedColor = CatalogPage.RowContent.Items.ItemsOptionalColors[0].Colors[0].ToString();
-            CatalogPage.RowContent.Items.ViewItemButton[0].Click<ViewItemPage>().ViewItemRow.PickedColor.ToString().Should().Be(pressedColor);
+            //save color's name by going to color filter and check what color by it's index 
+            var pressedColor = CatalogPage.RowContent.Items.OptionalColorsOfItem[0].Colors[0].ToString();
+            //click on this color
+            CatalogPage.RowContent.Items.OptionalColorsOfItem[0].Colors[0].Click<CatalogPage>();
+            //the picked color should be the same
+            CatalogPage.RowContent.Items.ViewButtonOfItem[0].Click<ViewItemPage>().ViewItemRow.PickedColor.ToString().Should().Be(pressedColor);
         }
 
         [TestMethod]
         public void ColorFilterWorksTest()
         {
+            //go to color filter & save the color's name by it's index
             var pickedColor = CatalogPage.RowContent.Filters.ColorFilter[0].ToString();
+            //click on the color and save the page to use it
             var postCatalogPage = CatalogPage.RowContent.Filters.ColorFilter[0].Click<CatalogPage>();
-            var selectedColors = new List<string>();
+            //            var selectedColors = new List<string>();
             //foreach() ===> item- open it, check the selected color, add it to the list and close the quick view.
             //then, scan all of the items in the list and make sure they are all equal to the picked color string
             //it can be an assertion
